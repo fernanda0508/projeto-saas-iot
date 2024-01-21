3001a5c36b2cdb97fb69696a70cca3bcf28590d9
fernanda_dev <ferhcoelhonunes@gmail.com>
2024-01-20 20:03:05 -0400

    acrescentando hook form a todos os formulários

--- front-end/src/pages/configuracoes/placa/index.tsx
+++ front-end/src/pages/configuracoes/placa/index.tsx
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Typography, FormControl, InputLabel, Select, MenuItem, TextField, Button, Box, Paper } from '@mui/material';
+import { useForm, Controller } from "react-hook-form";
 import ProtectedRoute from '@/hooks/useRequireAuth';
 
+type InputForm = {
+  modeloPlaca: string;
+  ssid: string;
+  senhaWifi: string;
+}
+
 const ConfiguracaoPlaca: React.FC = () => {
-  const [modeloPlaca, setModeloPlaca] = useState('');
-  const [ssid, setSsid] = useState('');
-  const [senhaWifi, setSenhaWifi] = useState('');
+  const { control, handleSubmit, formState: { errors } } = useForm<InputForm>();
 
-  const handleModeloPlacaChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setModeloPlaca(event.target.value as string);
-  };
-
-  const handleSsidChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSsid(event.target.value);
-  };
-
-  const handleSenhaWifiChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSenhaWifi(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log({ modeloPlaca, ssid, senhaWifi });
+  const onSubmit = (data: InputForm) => {
+    console.log(data);
   };
 
   return (
@@ -37,47 +29,67 @@ const ConfiguracaoPlaca: React.FC = () => {
           <Typography component="h1" variant="h5">
             Configuração da Placa
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <FormControl fullWidth margin="normal">
-              <InputLabel id="modelo-placa-label">Modelo da Placa</InputLabel>
-              <Select
-                labelId="modelo-placa-label"
-                id="modelo-placa"
-                value={modeloPlaca}
-                label="Modelo da Placa"
-                onChange={handleModeloPlacaChange}
-              >
-                <MenuItem value="ESP32">ESP32</MenuItem>
-                {/* Adicione mais <MenuItem> aqui para outros modelos */}
-              </Select>
-            </FormControl>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="ssid"
-              label="SSID Wi-Fi"
-              name="ssid"
-              autoComplete="ssid"
-              autoFocus
-              value={ssid}
-              onChange={handleSsidChange}
+          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+            <Controller
+              name="modeloPlaca"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <FormControl fullWidth margin="normal">
+                  <InputLabel id="modelo-placa-label">Modelo da Placa</InputLabel>
+                  <Select
+                    {...field}
+                    labelId="modelo-placa-label"
+                    id="modelo-placa"
+                    label="Modelo da Placa"
+                    error={!!errors.modeloPlaca}
+                  >
+                    <MenuItem value="ESP32">ESP32</MenuItem>
+                    {/* Adicione mais <MenuItem> aqui para outros modelos */}
+                  </Select>
+                </FormControl>
+              )}
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
+            <Controller
+              name="ssid"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="ssid"
+                  label="SSID Wi-Fi"
+                  name="ssid"
+                  autoComplete="ssid"
+                  autoFocus
+                  error={!!errors.ssid}
+                />
+              )}
+            />
+            <Controller
               name="senhaWifi"
-              label="Senha Wi-Fi"
-              type="password"
-              id="senha-wifi"
-              autoComplete="current-password"
-              value={senhaWifi}
-              onChange={handleSenhaWifiChange}
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="senhaWifi"
+                  label="Senha Wi-Fi"
+                  type="password"
+                  id="senha-wifi"
+                  autoComplete="current-password"
+                  error={!!errors.senhaWifi}
+                />
+              )}
             />
             <Box sx={{ display: 'flex', justifyContent: 'end', mt: 2 }}>
-
-              <Button variant="contained" sx={{}}>Próximo</Button>
+              <Button type="submit" variant="contained">Próximo</Button>
             </Box>
           </Box>
         </Box>
--- front-end/src/pages/configuracoes/protocolo/index.tsx
+++ front-end/src/pages/configuracoes/protocolo/index.tsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
 import { Grid, Container, Typography, TextField, Button, Box, Paper, Divider } from '@mui/material';
 import ProtectedRoute from '@/hooks/useRequireAuth';
+import { useAuth } from "@/contexts/AuthContext";
+import { useForm } from 'react-hook-form';
+
+// Tipagem para os dados do formulário
+interface IFormInput {
+  hostMqtt: string;
+  portaMqtt: string;
+  topicoMqtt: string;
+  usuarioMqtt: string;
+  senhaMqtt: string;
+  ssid: string;
+  senhaWifi: string;
+}
 
 const ConfiguracoesMqttWifi: React.FC = () => {
-  const [hostMqtt, setHostMqtt] = useState('');
-  const [portaMqtt, setPortaMqtt] = useState('');
-  const [topicoMqtt, setTopicoMqtt] = useState('');
-  const [usuarioMqtt, setUsuarioMqtt] = useState('');
-  const [senhaMqtt, setSenhaMqtt] = useState('');
-  const [ssid, setSsid] = useState('');
-  const [senhaWifi, setSenhaWifi] = useState('');
+  const { user } = useAuth();
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+
+  const onSubmit = (data: IFormInput) => {
+    console.log(data);
     // Trate o envio do formulário aqui
-    console.log({
-      hostMqtt,
-      portaMqtt,
-      topicoMqtt,
-      usuarioMqtt,
-      senhaMqtt,
-      ssid,
-      senhaWifi,
-    });
   };
 
   return (
     <ProtectedRoute>
       <Container component="main" maxWidth="md">
-        <Paper elevation={3} sx={{ p: 4 }}>
-          <Typography variant="h5" align="center" sx={{ mb: 6 }} gutterBottom>
-            Configurações MQTT e Wi-Fi
-          </Typography>
-          <form onSubmit={handleSubmit}>
+        <Paper elevation={3} sx={{ p: 4 }} >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Typography variant="h5" align="center" sx={{ mb: 6 }} gutterBottom>
+              Configurações MQTT e Wi-Fi
+            </Typography>
             <Grid container spacing={3}>
               <Grid item xs={12} md={6}>
                 <Typography variant="subtitle1" gutterBottom sx={{ fontWeight: 'bold' }}>
@@ -41,39 +41,52 @@ const ConfiguracoesMqttWifi: React.FC = () => {
                 <TextField
                   fullWidth
                   label="Host MQTT"
-                  value={hostMqtt}
-                  onChange={(e) => setHostMqtt(e.target.value)}
                   margin="normal"
+                  {...register('hostMqtt', { required: 'Host MQTT é obrigatório' })}
                 />
                 <TextField
                   fullWidth
                   label="Porta MQTT"
-                  value={portaMqtt}
-                  onChange={(e) => setPortaMqtt(e.target.value)}
                   margin="normal"
+                  {...register('portaMqtt', { required: 'Porta MQTT é obrigatório' })}
+
                 />
                 <TextField
                   fullWidth
                   label="Tópico MQTT"
-                  value={topicoMqtt}
-                  onChange={(e) => setTopicoMqtt(e.target.value)}
                   margin="normal"
+                  {...register('topicoMqtt', { required: 'Tópico MQTT é obrigatório' })}
+
                 />
                 <TextField
                   fullWidth
                   label="Nome de Usuário MQTT"
-                  value={usuarioMqtt}
-                  onChange={(e) => setUsuarioMqtt(e.target.value)}
                   margin="normal"
+                  value={user?.username}
+                  {...register('usuarioMqtt', { required: 'Usuário MQTT é obrigatório' })}
+                  InputProps={{
+                    readOnly: true,
+                  }}
                 />
+                <Typography variant="caption" style={{ color: 'gray' }}>
+                  O usuário definido aqui é o mesmo utilizado no seu cadastro.
+                </Typography>
                 <TextField
                   fullWidth
                   label="Senha MQTT"
                   type="password"
-                  value={senhaMqtt}
-                  onChange={(e) => setSenhaMqtt(e.target.value)}
+                  value={"**********"}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
                   margin="normal"
+                  InputProps={{
+                    readOnly: true,
+                  }}
                 />
+                <Typography variant="caption" style={{ color: 'gray' }}>
+                  A senha definida aqui é a mesma utilizada no seu cadastro.
+                </Typography>
               </Grid>
 
               <Divider orientation="vertical" flexItem sx={{ mx: 2 }} />
@@ -85,17 +98,15 @@ const ConfiguracoesMqttWifi: React.FC = () => {
                 <TextField
                   fullWidth
                   label="SSID"
-                  value={ssid}
-                  onChange={(e) => setSsid(e.target.value)}
                   margin="normal"
+                  {...register('ssid', { required: 'SSID é obrigatório' })}
                 />
                 <TextField
                   fullWidth
                   label="Senha Wi-Fi"
                   type="password"
-                  value={senhaWifi}
-                  onChange={(e) => setSenhaWifi(e.target.value)}
                   margin="normal"
+                  {...register('senhaWifi', { required: 'Senha do Wi-fi é obrigatório' })}
                 />
               </Grid>
             </Grid>
